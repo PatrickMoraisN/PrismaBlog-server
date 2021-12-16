f8745575f25bde86c989b358cf0c8c7955175a9b
Patrick Morais <ppternunes@gmail.com>
2021-12-16 11:05:56 -0300

    create EnsureAuthenticate middleware

---
+++ src/middlewares/EnsureAuthenticate.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+export interface IRequest extends Request {
+  id: string;
+}
+
+interface IVerifyPayload {
+  sub: string;
+}
+
+class EnsureAuthenticate {
+  ensure(request: IRequest, response: Response, next: NextFunction) {
+    const token = request.headers.authorization;
+
+    if (!token) {
+      return response.status(404).json({ message: 'invalid token' });
+    }
+
+    const [, authToken] = token.split(' ');
+
+    try {
+      const { sub } = verify(
+        authToken,
+        process.env.JWT_SECRET
+      ) as IVerifyPayload;
+
+      request.id = sub;
+
+      return next();
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
+}
+
+export { EnsureAuthenticate };
