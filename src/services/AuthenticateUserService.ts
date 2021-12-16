50b7be4f380042378a7af868e85d47967c374f08
Patrick Morais <ppternunes@gmail.com>
2021-12-16 10:43:36 -0300

    create Authenticate User service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { PrismaClient } from '.prisma/client';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IRequest) {
+    if (!email || !password) {
+      throw Error('Email / Password is required!');
+    }
+
+    const prisma = new PrismaClient();
+
+    const userExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!userExists) {
+      throw Error('Email / Password invalid!');
+    }
+
+    const isAValidPassword = compare(password, userExists.password);
+
+    if (!isAValidPassword) {
+      throw Error('Email / Password invalid!');
+    }
+
+    const JWT_SECRET_HASH = process.env.JWT_SECRET;
+
+    const token = sign({ email: userExists.email }, JWT_SECRET_HASH, {
+      subject: userExists.id,
+      expiresIn: '1d',
+    });
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
