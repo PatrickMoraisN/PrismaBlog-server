08bebe96046b149f910ae55e95eace95b937823f
Patrick Morais <ppternunes@gmail.com>
2021-12-16 11:35:50 -0300

    fix create user routes / add create post router

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreatePostController } from './controllers/CreatePostController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { EnsureAuthenticate } from './middlewares/EnsureAuthenticate';
 
@@ -7,12 +8,18 @@ const router = Router();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const ensureAuthenticate = new EnsureAuthenticate();
+const createPostController = new CreatePostController();
 
 router.get('/', (_request: Request, response: Response) => {
   return response.json({ ok: true });
 });
 
-router.post('/create', createUserController.handle);
+router.post('/register', createUserController.handle);
 router.post('/login', authenticateUserController.handle);
+router.post(
+  '/create/post',
+  ensureAuthenticate.ensure,
+  createPostController.handle
+);
 
 export { router };
