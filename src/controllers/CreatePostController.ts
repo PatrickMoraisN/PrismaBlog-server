5fa59f62933b5dd54dada07e4f4ba3242f7b1f20
Patrick Morais <ppternunes@gmail.com>
2021-12-16 11:33:57 -0300

    create CreatePostController

---
+++ src/controllers/CreatePostController.ts
@@ -0,0 +1,22 @@
+import { Response } from 'express';
+import { IRequest } from '../middlewares/EnsureAuthenticate';
+import { CreatePostService } from '../services/CreatePostService';
+
+class CreatePostController {
+  async handle(request: IRequest, response: Response) {
+    const { title, body } = request.body;
+    const { user_id } = request;
+
+    const createPostService = new CreatePostService();
+
+    const post = await createPostService.execute({
+      title,
+      body,
+      authorId: user_id,
+    });
+
+    return post;
+  }
+}
+
+export { CreatePostController };
